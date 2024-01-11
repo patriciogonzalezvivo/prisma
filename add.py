7bbb3927c5c824276399e1d304f54765c4ac6434
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-01-11 04:44:36 -0500

    more generalize add function

--- add.py
+++ add.py
@@ -37,13 +37,17 @@ import warnings
 warnings.filterwarnings("ignore")
 
 # Run band model
-def run(band, input_folder, output_file="", extra_args = "", force=False):
+def run(band, input_folder, output_file="", save_frames=False, extra_args = ""):
     print("\n# ", band.upper())
     cmd = "CUDA_VISIBLE_DEVICES=0 python3 bands/" + band + ".py -i " + input_folder
     if output_file != "":
         cmd += " --output " + output_file 
     if extra_args != "":
         cmd += " " + extra_args
+
+    if save_frames:
+        cmd += " --subpath " + band
+
     print(cmd,"\n")
     os.system(cmd)
 
@@ -53,8 +57,12 @@ if __name__ == '__main__':
     parser.add_argument('--input', '-i', help="input file", type=str, required=True)
     parser.add_argument('--output', help="folder name", type=str, default='')
     parser.add_argument('--fps', '-r', help='fix framerate', type=float, default=24)
-    parser.add_argument('--rgbd', help='Where the depth is', type=str, default=None)
-    parser.add_argument('--force', '-f', help='Force to run all bands', action='store_true')
+    parser.add_argument('--rgbd', '-d', help='Where the depth is', type=str, default=None)
+    parser.add_argument('--frames', '-f', help='Save extra frame data as images', action='store_true')
+    parser.add_argument('--ply', '-p', help='Save ply for images', action='store_true')
+    parser.add_argument('--npy', '-n', help='Save npy version of files', action='store_true')
+
+
     args = parser.parse_args()
 
     # 1. Get input basic parameters
@@ -82,14 +90,14 @@ if __name__ == '__main__':
     # 3. Extract RGBA (only if doesn't exist)
     add_band(data, "rgba", url=name_rgba)
 
-    extra_args = ""
+    extra_args = "--subpath images"
     if args.rgbd:
         extra_args += "--rgbd " + args.rgbd
 
     if is_video(input_path):
         extra_args += " -d images --fps " + str(args.fps)
 
-    run("rgba", input_path, path_rgba, extra_args=extra_args)
+    run("rgba", input_path, path_rgba, save_frames=True, extra_args=extra_args)
 
     # 4. Add metadata
     if is_video(input_path):
@@ -104,28 +112,36 @@ if __name__ == '__main__':
     # 5. Extract bands
     # 
 
+    extra_args = ""
+    if args.ply:
+        extra_args = "--ply "
+
+    if args.npy:
+        extra_args += "--npy "
+
+
     # Depth (MariGold)
-    run("depth_marigold", folder_name, extra_args="--ply")
+    run("depth_marigold", folder_name, save_frames=args.frames, extra_args=extra_args)
 
     # Depth HUE (ZoeDepth w MiDAS v2.1)
-    run("depth_zoedepth", folder_name, extra_args="--ply")
+    run("depth_zoedepth", folder_name, save_frames=args.frames, extra_args=extra_args)
 
     # Midas v3.1
-    run("depth_midas", folder_name, extra_args="--ply")
+    run("depth_midas", folder_name, save_frames=args.frames, extra_args=extra_args)
 
     # Mask (mmdet)
-    run("mask_mmdet", folder_name, extra_args="--sdf")
+    run("mask_mmdet", folder_name, save_frames=args.frames, extra_args=extra_args + "--sdf")
 
     if is_video(input_path):
         # Depth (PatchFusion w ZoeDepth)
-        run("depth_patchfusion", folder_name, extra_args="--mode=p49")
+        run("depth_patchfusion", folder_name, save_frames=args.frames, extra_args=extra_args + " --mode=p49")
 
         # Flow (RAFT)
         run("flow_raft", folder_name)
 
     else:        
         # Depth (PatchFusion w ZoeDepth)
-        run("depth_patchfusion", folder_name, extra_args="--ply")
+        run("depth_patchfusion", folder_name, extra_args=extra_args)
 
 
         
