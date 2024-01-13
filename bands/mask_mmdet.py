283d6b84a7526c84af0c73b9a8fcc664f67c38aa
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2024-01-13 11:34:21 -0500

    impruving mask band

--- bands/mask_mmdet.py
+++ bands/mask_mmdet.py
@@ -53,6 +53,8 @@ CONFIG = 'models/solov2_r101_fpn_3x_coco.py'
 MODEL  = 'models/solov2_r101_fpn_3x_coco_20220511_095119-c559a076.pth'
 # CLASSES = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 CLASSES = ['person', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe']
+CONFIDENCE_THRESHOLD = 0.7
+
 
 data = None
 model = None
@@ -64,7 +66,11 @@ def init_model():
     model = init_detector(CONFIG, MODEL, device=device)
 
 
-def getTotalMasks(result, category=0, conf=0.2):
+def getConfidence(result, category, index):
+    return result[0][category][index][4]
+
+
+def getTotalMasks(result, category=0, conf=CONFIDENCE_THRESHOLD):
     total = 0;
     for mask in result[0][category]:
         if mask[4] > conf:
@@ -81,18 +87,19 @@ def getMaskRGB(result, category, index):
     return np.stack([masks] * 3, axis=-1)
 
 
+def getSDF(masks):
+    mask = snowy.rgb_to_luminance( snowy.extract_rgb(masks) )
+    sdf = snowy.generate_sdf(mask != 0.0)
+    sdf = (sdf + 127.0) / 255.0
+    sdf = (sdf - 0.25) * 2.0
+    return 1.0-np.clip(sdf, 0.0, 1.0)
+
+
 def process_image(args):
     global model, device
 
     # Export properties
-    output_folder = os.path.dirname(args.output)
-    output_filename = os.path.basename(args.output)
-    output_basename = output_filename.rsplit(".", 1)[0]
-    output_extension = output_filename.rsplit(".", 1)[1]
-
-    sdf_filename = output_basename + "_sdf." + output_extension
-    sdf_path = output_folder + "/" + sdf_filename
-    
+    output_filename = os.path.basename(args.output)    
     img = cv2.imread(args.input)
 
     # run the inferance
@@ -103,27 +110,22 @@ def process_image(args):
     for c in range(total_categories):
         category = model.CLASSES[c]
         total_masks = getTotalMasks(result, c)
+        if category in CLASSES:
+            for i in range(total_masks):
+                if getConfidence(result, c, i) > args.confidence:
+                    mask = getMaskRGB(result, c, i)
+                    masks = masks + mask
         
-        for i in range(total_masks):
-            mask = getMaskRGB(result, c, i)
-            if category in CLASSES:
-                masks = masks + mask
-        
+    # SDF
+    if args.sdf:
+        sdf = getSDF(masks)
+        masks[..., 1] = sdf[...,0] * 255
+
     # Mask
     cv2.imwrite(args.output, masks.astype(np.uint8))
     data["bands"][BAND] = { }
     data["bands"][BAND]["url"] = output_filename
     data["bands"][BAND]["ids"] = CLASSES
-
-    # SDF
-    if args.sdf:
-        mask = snowy.rgb_to_luminance( snowy.extract_rgb(masks) )
-        sdf = snowy.generate_sdf(mask != 0.0)
-        sdf = (sdf + 127.0) / 255.0
-        sdf = (sdf - 0.25) * 2.0
-        snowy.export(sdf, sdf_path)
-        data["bands"][BAND + "_sdf"] = { }
-        data["bands"][BAND + "_sdf"]["url"] = sdf_filename
 
 
 def process_video(args):
@@ -146,8 +148,6 @@ def process_video(args):
     output_path = args.output
     output_folder = os.path.dirname(output_path)
     output_filename = os.path.basename(output_path)
-    output_basename = output_filename.rsplit(".", 1)[0]
-    output_extension = output_filename.rsplit(".", 1)[1]
         
     mask_video = VideoWriter(width=width, height=height, frame_rate=fps, filename=output_path )
 
@@ -157,20 +157,6 @@ def process_video(args):
         args.subpath = os.path.join(output_folder, args.subpath)
         create_folder(args.subpath)
 
-    if args.sdf_subpath != '':
-        args.sdf = True
-
-    if args.sdf:
-        sdf_filename = output_basename + "_sdf." + output_extension
-        sdf_path = output_folder + "/" + sdf_filename
-        sdf_video = VideoWriter(width=width, height=height, frame_rate=fps, filename=sdf_path )
-
-        if args.sdf_subpath != '':
-            if data:
-                data["bands"][BAND + "_sdf"]["folder"] = args.sdf_subpath
-            args.sdf_subpath = os.path.join(output_folder, args.sdf_subpath)
-            create_folder(args.sdf_subpath)
-
     for f in tqdm( range(total_frames) ):
         img = cv2.cvtColor(in_video[f].asnumpy(), cv2.COLOR_BGR2RGB)
         result = inference_detector(model, img)
@@ -179,28 +165,20 @@ def process_video(args):
         for c in range(total_categories):
             category = model.CLASSES[c]
             total_masks = getTotalMasks(result, c)
-            color = hue_to_rgb( float(c) / float(total_categories) )
-            for i in range(total_masks):
-                mask = getMaskRGB(result, c, i)
-                if category in CLASSES:
-                    masks = masks + mask
+            if category in CLASSES:
+                for i in range(total_masks):
+                    if getConfidence(result, c, i) > args.confidence:
+                        mask = getMaskRGB(result, c, i)
+                        masks = masks + mask
+        
+        if args.sdf:
+            sdf = getSDF(masks)
+            masks[..., 1] = sdf[...,0] * 255
 
         mask_video.write( masks.astype(np.uint8) )
 
         if args.subpath != '':
             cv2.imwrite(os.path.join(args.subpath, "{:05d}.png".format(f)), masks.astype(np.uint8))
-
-        if args.sdf:
-            masks = snowy.rgb_to_luminance( snowy.extract_rgb(masks) )
-            sdf = snowy.generate_sdf(masks != 0.0)
-            sdf = (sdf + 127.0) / 255.0
-            sdf = np.clip((sdf - 0.25) * 2.0, 0.0, 1.0)
-            sdf *= 255
-            sdf = cv2.merge((sdf,sdf,sdf))
-            sdf_video.write( sdf.astype(np.uint8) )
-
-            if args.sdf_subpath != '':
-                cv2.imwrite(os.path.join(args.sdf_subpath, "{:05d}.png".format(f)), sdf.astype(np.uint8))
 
     # Mask
     mask_video.close()
@@ -208,23 +186,16 @@ def process_video(args):
     data["bands"][BAND]["url"] = output_filename
     data["bands"][BAND]["ids"] = CLASSES
 
-    # SDF
-    if args.sdf:
-        sdf_video.close()
-        data["bands"][BAND + "_sdf"] = { }
-        data["bands"][BAND + "_sdf"]["url"] = sdf_filename
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--input', '-i', help="input", type=str, required=True)
     parser.add_argument('--output', '-o', help="output", type=str, default="")
-
-    parser.add_argument('--subpath', help="Mask Subpath to frames", type=str, default='')
+    parser.add_argument('--confidence', '-c', help="confidence threshold", type=float, default=CONFIDENCE_THRESHOLD)
 
     parser.add_argument('--sdf', '-s',action='store_true')
-    parser.add_argument('--sdf_subpath', help="SDF Subpath to frames", type=str, default='')
+    parser.add_argument('--subpath', help="Mask Subpath to frames", type=str, default='')
 
     args = parser.parse_args()
 
